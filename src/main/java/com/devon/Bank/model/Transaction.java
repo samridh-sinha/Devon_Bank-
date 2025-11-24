f76e2d1f966c798ed88ed47f419c68b13fc7e8dc
Samridh Sinha <samridh863@gmail.com>
2025-11-24 12:52:11 +0530

    Adds lombok annotations

--- src/main/java/com/devon/Bank/model/Transaction.java
+++ src/main/java/com/devon/Bank/model/Transaction.java
@@ -7,82 +7,18 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Transaction {
     private long id;
     private long accountId;
     private BigDecimal amount;
     private String txnType;
     private LocalDateTime txnTimeStamp;
-
     private String channel;
     private String status;
     private String description;
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(long accountId) {
-        this.accountId = accountId;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getTxnType() {
-        return txnType;
-    }
-
-    public void setTxnType(String txnType) {
-        this.txnType = txnType;
-    }
-
-    public LocalDateTime getTxnTimeStamp() {
-        return txnTimeStamp;
-    }
-
-    public void setTxnTimeStamp(LocalDateTime txnTimeStamp) {
-        this.txnTimeStamp = txnTimeStamp;
-    }
-
-    public String getChannel() {
-        return channel;
-    }
-
-    public void setChannel(String channel) {
-        this.channel = channel;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
